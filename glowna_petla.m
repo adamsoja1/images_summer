ff1783a76a732b63b87c4e5786b66ccbb1d924e8
Rebek2 <74105281+Rebek2@users.noreply.github.com>
2022-07-19 09:10:31 +0200

    new photos etc

--- glowna_petla.m
+++ glowna_petla.m
@@ -8,8 +8,6 @@ function [figures] = glowna_petla(numer,ilosc,t,seria,sciezka)
         obraz = imread(sciezka + "/IMG0" + num2str(wartosc) + ".JPG");
         w = srednia(obraz);
         plot(i,w(1),'r*',i,w(2),'g*',i,w(3),'b*')
-            ylim([0 3])
-
         title('Dla wartosci centrum 100x100')
         hold on;
         grid on;
@@ -26,7 +24,7 @@ function [figures] = glowna_petla(numer,ilosc,t,seria,sciezka)
         obraz1 = imread(sciezka + "/IMG0" + num2str(wartosc1) + ".JPG");
         w_kat = srednia_kat(obraz1);
         plot(i,w_kat(1),'r*',i,w_kat(2),'g*',i,w_kat(3),'b*')
-            ylim([0 3])
+        
 
         title('Dla wartosci lewego gornego rogu(100x100)')
         hold on;
@@ -36,51 +34,51 @@ function [figures] = glowna_petla(numer,ilosc,t,seria,sciezka)
         wynikG_kat(i) = w_kat(2);
     
     end
-    length(e)
-  
-    figure()
-    plot(e,wynikR(1:length(e)),'r*', e,wynikG(1:length(e)),'g*', e,wynikB(1:length(e)),'b*')
-    xlabel('Czas log_2(t/t_0)')
-    ylabel('Wartość pikseli')
-   
-    
-%     j=1;
-%     for i=1:length(t)
-% 
-%       if mod(i,2) ~= 0
-% 
-%         wynikR_R1 = mean(wynikR(i:i+1));
-%         wynikG_G2 =  mean(wynikG(i:i+1)); 
-%         wynikB_B3 =  mean(wynikB(i:i+1)); 
+%     length(e)
+%   
+%     figure()
+%     plot(e,wynikR,'r*', e,wynikG,'g*', e,wynikB,'b*')
+%     xlabel('Czas log_2(t/t_0)')
+%     ylabel('Wartość pikseli')
+%    
 %     
-%         wynikR_R_k1 = mean(wynikR_kat(i:i+1)); 
-%         wynikG_G_k2 =  mean(wynikG_kat(i:i+1)); 
-%         wynikB_B_k3 =  mean(wynikB_kat(i:i+1)); 
-%       end
-%         
-%         wynikR_R(i) = wynikR_R1
-%         wynikG_G(i) =  wynikG_G2
-%         wynikB_B(i) =  wynikB_B3
-%     
-%         wynikR_R_k(i) = wynikR_R_k1
-%         wynikG_G_k(i) =  wynikG_G_k2
-%         wynikB_B_k(i) =  wynikB_B_k3
-%       
-%       
-%     end
-        j=1;
+    j=1;
     for i=1:length(t)
+
+      if mod(i,2) ~= 0
+
+        wynikR_R1 = mean(wynikR(i:i+1));
+        wynikG_G2 =  mean(wynikG(i:i+1)); 
+        wynikB_B3 =  mean(wynikB(i:i+1)); 
+    
+        wynikR_R_k1 = mean(wynikR_kat(i:i+1)); 
+        wynikG_G_k2 =  mean(wynikG_kat(i:i+1)); 
+        wynikB_B_k3 =  mean(wynikB_kat(i:i+1)); 
+      end
+        
+        wynikR_R(i) = wynikR_R1
+        wynikG_G(i) =  wynikG_G2
+        wynikB_B(i) =  wynikB_B3
+    
+        wynikR_R_k(i) = wynikR_R_k1
+        wynikG_G_k(i) =  wynikG_G_k2
+        wynikB_B_k(i) =  wynikB_B_k3
       
-        wynikR_R(i) = mean(wynikR(j:i*10));
-        wynikG_G(i) =  mean(wynikG(j:i*10)); 
-        wynikB_B(i) =  mean(wynikB(j:i*10)); 
-    
-        wynikR_R_k(i) = mean(wynikR_kat(j:i*10)); 
-        wynikG_G_k(i) =  mean(wynikG_kat(j:i*10)); 
-        wynikB_B_k(i) =  mean(wynikB_kat(j:i*10)); 
-    
-        j = j + seria;
+      
     end
+%         j=1;
+%     for i=1:length(t)
+%       
+%         wynikR_R(i) = mean(wynikR(j:i*10));
+%         wynikG_G(i) =  mean(wynikG(j:i*10)); 
+%         wynikB_B(i) =  mean(wynikB(j:i*10)); 
+%     
+%         wynikR_R_k(i) = mean(wynikR_kat(j:i*10)); 
+%         wynikG_G_k(i) =  mean(wynikG_kat(j:i*10)); 
+%         wynikB_B_k(i) =  mean(wynikB_kat(j:i*10)); 
+%     
+%         j = j + seria;
+%     end
 
     
     figures(3) = figure();
--- untitled.m
+++ untitled.m
@@ -1,8 +1,7 @@
 clc;
 clear all;
 close all;
-t = 15:10:105;
-seria = 10;
+
 %max swiatlo az do wysycenia
 %glowna_petla(149,100,15:10:105,seria,"zdj_day1")
 %prad ciemny
@@ -15,12 +14,13 @@ seria = 10;
 %prad ciemny
 %glowna_petla(760,100,1:100:901,10,"zdj_day_black")
 
-%glowna_petla(1400,706,1:2:706,2,"zdj_day4_2")
-t2 = [1:10:901];
+glowna_petla(1400,706,1:2:706,2,"zdj_day4_2")
+
 %prad ciemny
 %glowna_petla(760,100,t2,seria,"zdj_day_black")
 
-
+%prad ciemny day6
+%glowna_petla(2127,100,0:100:10000,1,'zdj_day5')
 
 %prad ciemny
-glowna_petla(2229,100,1:100:10000,1,'zdj_day6_black')
+%glowna_petla(230,20,0:100:1900,1,'proba_day6')
